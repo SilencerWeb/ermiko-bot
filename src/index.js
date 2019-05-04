6308139bea45c8181baffe377d842292b3d4062b
Maksim Gorodov <silencer.web@gmail.com>
2019-05-04 13:39:16 +0500

    Add a function for sending posts and modify a function for generating JSON

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
 
 const fs = require('fs');
+
+const Telegram = require('telegraf/telegram');
 const axios = require('axios');
+
+
+const telegram = new Telegram(process.env.BOT_TOKEN);
 
 
 const getDateAsString = () => {
@@ -19,11 +24,10 @@ const getDateAsString = () => {
 };
 
 const generateJSON = () => {
-  const formattedPosts = [];
-
-  axios.get('https://www.reddit.com/r/aww/top/.json?t=day&limit=100')
+  return axios.get('https://www.reddit.com/r/aww/top/.json?t=day&limit=100')
     .then((response) => {
       const posts = response.data.data.children;
+      const formattedPosts = [];
 
       posts.forEach((post) => {
         const formattedPost = {};
@@ -35,7 +39,9 @@ const generateJSON = () => {
         if (post.data.post_hint === 'link') {
           if (post.data.crosspost_parent_list) {
             postData = post.data.crosspost_parent_list[0];
-            postHint = postData.post_hint === 'link' ? 'image' : postData.post_hint;
+            postHint = postData.post_hint === 'link' ? postData.domain === 'i.imgur.com' ? 'rich:video' : 'image' : postData.post_hint;
+          } else if (post.data.domain === 'i.imgur.com') {
+            postHint = 'rich:video';
           } else {
             postHint = 'image';
           }
@@ -52,7 +58,9 @@ const generateJSON = () => {
             break;
           case 'rich:video':
             formattedPost.type = 'video';
-            if (postData.preview.enabled === true) formattedPost.link = postData.preview.reddit_video_preview.fallback_url;
+            if (postData.preview.reddit_video_preview && postData.preview.reddit_video_preview.fallback_url) {
+              formattedPost.link = postData.preview.reddit_video_preview.fallback_url;
+            }
             break;
         }
 
@@ -65,15 +73,56 @@ const generateJSON = () => {
         formattedPosts.push(formattedPost);
       });
 
-      return formattedPosts;
+      return { posts, formattedPosts };
     })
-    .then((formattedPosts) => {
+    .then(({ posts, formattedPosts }) => {
+      const postsAsJSON = JSON.stringify(posts);
       const formattedPostsAsJSON = JSON.stringify(formattedPosts);
 
-      const fileName = `posts-${getDateAsString()}.json`;
+      const postsFileName = `posts-${getDateAsString()}.json`;
+      const formattedPostsFileName = `formatted-posts-${getDateAsString()}.json`;
 
-      fs.writeFile(fileName, formattedPostsAsJSON, 'utf8', () => {
-        console.log(`${fileName} was successfully created`);
+      fs.writeFile(postsFileName, postsAsJSON, 'utf8', () => {
+        console.log(`${postsFileName} was successfully created`);
+      });
+
+      fs.writeFile(formattedPostsFileName, formattedPostsAsJSON, 'utf8', () => {
+        console.log(`${formattedPostsFileName} was successfully created`);
       });
     });
+};
+
+const sendPosts = () => {
+  const formattedPostsFileName = `formatted-posts-${getDateAsString()}.json`;
+
+  fs.access(formattedPostsFileName, fs.F_OK, (error) => {
+    if (error) {
+      generateJSON().then(() => sendPosts());
+    } else {
+      fs.readFile(formattedPostsFileName, 'utf8', (error, posts) => {
+        if (error) {
+          console.log(error);
+        } else {
+          const postsAsArray = JSON.parse(posts);
+
+          postsAsArray.forEach((post) => {
+            const postTitle = post.title;
+            const postLink = post.link;
+
+            if (post.type === 'video') {
+              telegram.sendVideo(554773669, postLink, { caption: postTitle }).catch((error) => {
+                console.log(post);
+                console.log(error);
+              });
+            } else if (post.type === 'image') {
+              telegram.sendPhoto(554773669, postLink, { caption: postTitle }).catch((error) => {
+                console.log(post);
+                console.log(error);
+              });
+            }
+          });
+        }
+      });
+    }
+  });
 };
