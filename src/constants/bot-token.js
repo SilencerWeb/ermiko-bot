8e9595edc69c784da320a670ece5e027a109d37a
Maksim Gorodov <silencer.web@gmail.com>
2019-06-22 10:24:31 +0500

    Fix a bug with checking if it is a production environment or not

--- src/constants/bot-token.js
+++ src/constants/bot-token.js
@@ -1,4 +1,4 @@
-const { IS_PRODUCTION } = require('./');
+const { IS_PRODUCTION } = require('./is-production');
 
 
 const BOT_TOKEN = IS_PRODUCTION ? process.env.BOT_TOKEN_PRODUCTION : process.env.BOT_TOKEN_DEVELOPMENT;
