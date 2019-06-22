57041177220da3ce41a0c841eed77ca5eccf4ced
Maksim Gorodov <silencer.web@gmail.com>
2019-06-22 20:44:29 +0500

    Make a cron job for publishing posts work also every 30th minute between 3:00 and 15:00 UTC (8:00 and 20:00 GMT+5, Tashkent)

--- src/cron/register-publishing-posts-cron-job.js
+++ src/cron/register-publishing-posts-cron-job.js
@@ -22,22 +22,28 @@ const publishRandomApprovedPost = async (channel) => {
 
 
 const registerPublishingPostsCronJob = () => {
+  const CRON_JOB_TIMES = [
+    '0 0 * * * *', // Every 0th minute
+    '0 30 3-15 * * *', // Every 30th minute between 3:00 and 15:00 UTC (8:00 and 20:00 GMT+5, Tashkent)
+  ];
 
-  new CronJob('0 0 * * * *', () => { // Every hour
-    if (IS_PRODUCTION) {
-      Object.keys(CHANNELS_INFO).forEach((channel) => {
-        publishRandomApprovedPost(channel).catch((error) => {
-          console.log(`Error on publishing post to the channel "${channel}"!`);
+  CRON_JOB_TIMES.forEach((cronJobTime) => {
+    new CronJob(cronJobTime, () => {
+      if (IS_PRODUCTION) {
+        Object.keys(CHANNELS_INFO).forEach((channel) => {
+          publishRandomApprovedPost(channel).catch((error) => {
+            console.log(`Error on publishing post to the channel "${channel}"!`);
+            console.log(`Error message: ${error.message}`);
+          });
+        });
+      } else {
+        publishRandomApprovedPost(DEVELOPMENT_CHANNEL_ID).catch((error) => {
+          console.log(`Error on publishing post to the development channel!`);
           console.log(`Error message: ${error.message}`);
         });
-      });
-    } else {
-      publishRandomApprovedPost(DEVELOPMENT_CHANNEL_ID).catch((error) => {
-        console.log(`Error on publishing post to the development channel!`);
-        console.log(`Error message: ${error.message}`);
-      });
-    }
-  }, null, true);
+      }
+    }, null, true);
+  });
 };
 
 
