ba80129830c356c1c2f8e000ceaacd427ea95fd6
Maksim Gorodov <silencer.web@gmail.com>
2019-06-25 19:22:11 +0500

    Add removing dots at the end of the string in the function prettifyTitle

--- src/utils/prettify-title.js
+++ src/utils/prettify-title.js
@@ -1,5 +1,6 @@
 const prettifyTitle = (title) => {
   return title
+    .replace(/\.+$/, '') // Removes dots in the end of the string
     .replace(/&lt;/g, '<')
     .replace(/&gt;/g, '>')
     .replace(/&amp;/g, '&');
